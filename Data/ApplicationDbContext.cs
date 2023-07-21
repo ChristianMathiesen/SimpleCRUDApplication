4cdb1ffcc65e531c801b34279e479bd272e13563
Christian Mathiesen <cm1995@live.dk>
2023-07-21 22:27:30 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SimpleCRUDApplication.Models;
+
+namespace SimpleCRUDApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<SimpleCRUDApplication.Models.Joke>? Joke { get; set; }
+    }
+
+}
